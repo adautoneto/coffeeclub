fc59de265d641795b1a97db18c5f9d3cf348bac6
Adauto Neto <stanleystl@gmail.com>
2016-09-30 10:16:39 -0300

    Show statement and balance

---
+++ app.js
@@ -0,0 +1,13 @@
+(function() {
+	
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyB1Q7yiewsZu74A6QNzm_SjbMv1WoI3F5E",
+		authDomain: "coffeeclub-33a84.firebaseapp.com",
+		databaseURL: "https://coffeeclub-33a84.firebaseio.com",
+		storageBucket: "coffeeclub-33a84.appspot.com",
+		messagingSenderId: "204712776659"
+	};
+	firebase.initializeApp(config);
+
+}());
